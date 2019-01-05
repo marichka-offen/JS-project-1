382e8195216846ab1c633b6ed315133526f1e2af
Marichka Tsiuriak <marichka.offen@gmail.com>
2019-01-05 01:56:08 -0500

    Work on game logic
    - Create variables for score and turn counting and set them to 0;
    
    - Create on-click event and assign it a function responsible for
    score update and players switch.

--- app.js
+++ app.js
@@ -8,3 +8,52 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+let scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector(".dice").style.display = "none";
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  // 1 - Random number for a dice
+  let dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2 - Display the result
+  let diceDOM = document.querySelector(".dice");
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + dice + ".png";
+
+  // 3 - Update the round score if the rolled number is NOT a 1
+  if (dice !== 1) {
+    // Add score
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    // Switch to the next player
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    roundScore = 0;
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    document.querySelector(".dice").style.display = "none";
+
+    // document.querySelector('.player-0-panel').classList.remove('active')
+    // document.querySelector('.player-1-panel').classList.add('active')
+    // ^ Another way to interact with classes of the DOM elements ^
+  }
+});
+
+//document.querySelector("#current-" + activePlayer).textContent = dice; // setter
+// let x = document.querySelector('#score-0').textContent;               // getter
+// console.log(x);
